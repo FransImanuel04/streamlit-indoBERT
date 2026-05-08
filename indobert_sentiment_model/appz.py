f5dd6703c85e3d5d6a99551a48986d8972f26c31
FransImanuel04 <imanuelprans@gmail.com>
2026-05-08 22:39:13 +0700

    Add files via upload

--- indobert_sentiment_model/appz.py
+++ indobert_sentiment_model/appz.py
@@ -1,15 +1,82 @@
 import streamlit as st
-import os
+import torch
 
-st.write("Current directory:")
-st.write(os.getcwd())
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification
+)
 
-st.write("Files in current directory:")
-st.write(os.listdir())
+# Label mapping
+id2label = {
+    0: "negative",
+    1: "neutral",
+    2: "positive"
+}
 
-st.write("Files inside sentiment_model:")
+# Path model
+MODEL_PATH = "indobert_sentiment_model/sentiment_model"
 
-if os.path.exists("sentiment_model"):
-    st.write(os.listdir("sentiment_model"))
-else:
-    st.write("sentiment_model folder NOT FOUND")
+# Load model dan tokenizer
+@st.cache_resource
+def load_model():
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_PATH
+    )
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_PATH
+    )
+
+    model.eval()
+
+    return tokenizer, model
+
+# Loading spinner
+with st.spinner("Loading IndoBERT model..."):
+    tokenizer, model = load_model()
+
+# Title
+st.title("Sentiment Analysis IndoBERT")
+st.write("Analisis sentimen review Tokopedia")
+
+# Input user
+user_input = st.text_area(
+    "Masukkan Review"
+)
+
+# Tombol prediksi
+if st.button("Predict"):
+
+    if user_input.strip() != "":
+
+        # Tokenisasi
+        inputs = tokenizer(
+            user_input,
+            return_tensors="pt",
+            truncation=True,
+            padding=True,
+            max_length=128
+        )
+
+        # Prediksi
+        with torch.no_grad():
+
+            outputs = model(**inputs)
+
+            prediction = torch.argmax(
+                outputs.logits,
+                dim=1
+            ).item()
+
+        # Hasil prediksi
+        sentiment = id2label[prediction]
+
+        st.success(
+            f"Hasil Sentiment: {sentiment}"
+        )
+
+    else:
+        st.warning(
+            "Masukkan teks terlebih dahulu"
+        )
